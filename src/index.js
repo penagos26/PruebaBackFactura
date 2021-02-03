655a309f5b6e5aa7ddd03acce0b56b41b6baa268
penagos26 <75628346+penagos26@users.noreply.github.com>
2021-02-03 15:52:14 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 3000);
 
 // Procesamiento 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
