16e4e1c9192a276aa80a9c7436a615de81c33995
penagos26 <75628346+penagos26@users.noreply.github.com>
2021-02-20 11:16:52 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongo = require("mongoose");
 
 //const URI = 'mongodb://localhost/facturas-prueba'
-const URI = "mongodb+srv://Pruebas:contrasena01+@cluster0.glox7.mongodb.net/facturas?retryWrites=true&w=majority";
+const URI = "mongodb+srv://Pruebas:contrasena01@cluster0.glox7.mongodb.net/facturas?retryWrites=true&w=majority";
 
 mongo.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true } )
     .then(db => console.log('BD esta conectada'))
